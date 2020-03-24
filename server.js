218ae86d445d628c671e45ebe37d66736651dc74
ccrum292 <ccrum292@gmail.com>
2020-03-24 18:35:37 -0400

    wrote server.js and routes.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 8080;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+mongoose.connect(process.env.MONGOD_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+
+require("./controllers/routes")(app);
+
+
+app.listen(PORT, ()=> {
+  console.log(`App running on port ${PORT}`);
+});
